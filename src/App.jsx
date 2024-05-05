9b9f2e401fb8f96f3d55700b7d25a0b7a1e2b1ad
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-05-05 15:32:49 +0530

    bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline text-center text-red-600">
-      Hello world!
-    </h1>
+    <div className='uppercase'>netflix</div>
   );
 };
 export default App;
