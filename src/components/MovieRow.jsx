3a79186e01f470522480f8e60bf0b8cc8364772f
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-05-24 23:51:18 +0530

    added movie list

--- src/components/MovieRow.jsx
+++ src/components/MovieRow.jsx
@@ -1,4 +1,26 @@
-const MovieRow = () => {
-  return <div>MovieRow</div>;
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import MovieItem from './MovieItem';
+
+const MovieRow = ({ title, url }) => {
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    axios.get(url).then((response) => setMovies(response.data.results))
+  }, [url])
+  // console.log(movies);
+  
+  return (
+    <>
+    <h2 className='font-nsans-bold md:text-xl p-4 capitalize'>{title}</h2>
+    <div className='relative flex items-center'>
+      <div id={`slider`} className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide ">
+        {movies.map((movie) => (
+          <MovieItem key={movie.id} movie={movie}/>
+        ))}
+      </div>
+    </div>
+    </>
+  )
 };
 export default MovieRow;
