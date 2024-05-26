868af239df29b3e6ef903c19c42f0d067238958c
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-05-26 23:58:04 +0530

    Profile route path added

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,19 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Profile from "./pages/Profile";
-import Navbar from './components/Navbar';
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <>
-    <Navbar/>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/profile" element={<Profile />} />
-    </Routes>
-  </>
-  )
- 
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </>
+  );
 };
 export default App;
