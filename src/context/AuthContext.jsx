f81f0a0b30d0d7b54a2830affe214d6facc3b1d2
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-05-27 23:44:42 +0530

    methods creating of login and logout

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,13 +1,21 @@
-import { createContext, useContext } from 'react';
+import { createContext, useContext, useState } from 'react';
 import {createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged} from 'firebase/auth'
-import firebase from 'firebase/compat/app';
+import { auth, db } from '../services/Firebase';
+
 const AuthContext = createContext()
 
 export function AuthContextProvider({children}) {
     const [user, setUser] = useState({})
 
     function signUp(email, password) {
-        
+        createUserWithEmailAndPassword(auth , email, password)
+    }
+    function logIn(email, password){
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    function logOut(){
+        return signOut(auth)
     }
 
     return (
