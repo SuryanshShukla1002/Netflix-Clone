db96aeaf8d8438d5167b4e3400e2d68e29560b8b
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-06-02 22:27:44 +0530

    like button made in functional

--- src/components/MovieItem.jsx
+++ src/components/MovieItem.jsx
@@ -1,10 +1,26 @@
 import { useState } from 'react';
 import { createImageUrl } from "../services/movieServices";
 import {FaHeart, FaRegHeart} from 'react-icons/fa';
+import { doc, updateDoc, arrayUnion } from 'firebase/firestore';
+import {db} from '../services/Firebase'
 
 const MovieItem = ({ movie }) => {
     const [like, setLike] = useState(false)
   const { title, backdrop_path, poster_path } = movie;
+  const markFavShow = async () => {
+    const userEmail = user?.email
+
+    if(userEmail) {
+      const userDoc = doc(db, 'users' , userEmail)
+      setLike(!like)
+      await updateDoc(userDoc, {
+        favShows: arrayUnion({...movie})
+      })
+    }
+    else {
+      alert("Login to save a movie")
+    }
+  }
   return (
     <div className="relative w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block rounded-lg overflow-hidden cursor-pointer m-2">
       <img
@@ -14,7 +30,7 @@ const MovieItem = ({ movie }) => {
       />
       <div className='absolute top-0 left-0 w-full h-40 bg-black/80 opacity-0 hover:opacity-100'>
         <p className='whitespace-normal text-xs md:text-sm flex justify-center items-center h-full font-nsans-bold'>{movie.title}</p>
-        <p>{ like ? <FaHeart  size={20} className='absolute top-2 text-gray-300'/> : <FaRegHeart size={20} className='absolute top-2 text-gray-300' />}</p>
+        <p onClick={markFavShow} className='cursor-pointer'>{ like ? <FaHeart  size={20} className='absolute top-2 text-gray-300'/> : <FaRegHeart size={20} className='absolute top-2 text-gray-300' />}</p>
       </div>
     </div>
   );
