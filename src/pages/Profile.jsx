59fb1f9ace724b2594a395ad53cfb2316d2df1c0
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-06-03 15:12:26 +0530

    favorite movie showing on console

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -8,8 +8,21 @@ import { useEffect, useState } from 'react';
 
 const Profile = () => {
   const [movies, setMovies] = useState([])
+  const {user } = UserAuth();
 
-  useEffect(() => {}, )
+  useEffect(() => {
+    if (user) {
+      onSnapshot(doc(db , 'users', `${user.email}`), (doc) => {
+        if(doc.data()) setMovies(doc.data().favShows)
+      })
+    }
+  }, [user?.email])
+
+  // console.log(movies);
+
+  if (!user){
+    return <><p>Fetching Shows</p></>
+  }
 
   return <div>Profile</div>;
 };
