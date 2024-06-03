2dfecab9877b760e1267ff56d63e3bdff809374e
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-06-03 15:02:42 +0530

    all necissory things imported

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,4 +1,16 @@
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+import { AiOutlineClose } from 'react-icons/ai';
+import { UserAuth } from '../context/AuthContext';
+import { db } from '../services/Firebase';
+import { createImageUrl } from '../services/movieServices';
+import { arrayRemove, doc, onSnapshot, updateDoc } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
+
 const Profile = () => {
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {}, )
+
   return <div>Profile</div>;
 };
 export default Profile;
